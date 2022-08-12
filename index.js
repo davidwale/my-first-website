7892bfad7c9d622e8884ae9e883078dc00bc2bc2
davidwale <davidwale2003@gmail.com>
2022-08-12 20:40:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+
+
+
+const app = express();
+ 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/index.html")
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started at port 3000");
+} );
